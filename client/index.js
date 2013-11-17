26de5a62cbcc5c6485c80de5b4a31217ce62fd3e
Ionică Bizău <bizauionica@yahoo.com>
2013-11-17 21:04:22 +0200

    Try to get the email address. If it is not set, get it from the user.

--- client/index.js
+++ client/index.js
@@ -7,20 +7,30 @@
 // @copyright    2013, Ionică Bizău
 // ==/UserScript==
 
-/*
- *    <tr>
- *        <th></th>
- *        <td></td>
- *        <td>follow this user</td>
- *     </tr> 
- * */
+// try to get the logged user email adress
+var LOGGED_USER_ADRESS_EMAIL = localStorage["loggedUserAdressEmail"];
 
+// if it is undefined
+if (!LOGGED_USER_ADRESS_EMAIL) {
+
+    // get it from the user
+    LOGGED_USER_ADRESS_EMAIL = prompt("Enter your email address:", "");
+
+    // and set it into local storage
+    localStorage["loggedUserAdressEmail"] = LOGGED_USER_ADRESS_EMAIL;
+}
+
+// create the tbody to append into data table
 var $newTbody = $("<tbody>"),
     $tr 	  = $("<tr>");
 
+// create the row
 $tr.append("<th></th>");
 $tr.append("<td></td>");
 $tr.append("<td><a href='#' class='follow-me'>follow this user</a></td>");
 
+// append row to tbody
 $newTbody.append($tr);
+
+// append tbody to table
 $(".data:first > table").append($newTbody);
