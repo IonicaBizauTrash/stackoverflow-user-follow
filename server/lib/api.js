76fe5238ceba2f4de03e57965e464a958a09245c
Ionică Bizău <bizauionica@yahoo.com>
2013-11-17 10:29:01 +0200

    Adding server files.

---
+++ server/lib/api.js
@@ -0,0 +1,11 @@
+module.exports["/api/followUser/"] = function (req, res) {
+    res.end("Not yet implemented");
+};
+
+module.exports["/api/unfollowUser/"] = function (req, res) {
+    res.end("Not yet implemented");
+};
+
+module.exports["/api/isFollowing/"] = function (req, res) {
+    res.end("Not yet implemented");
+};
